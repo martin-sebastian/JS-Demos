a788d53b52cdb5a98c19438ae96650dd0365a6e2
Martin Sebastian <martin@martinsebastian.co>
2014-08-16 14:14:05 -0400

    Added Animation
    Added animation to to app to show different transitions for MVC

--- angular/app.js
+++ angular/app.js
@@ -1,30 +1,52 @@
 'use strict';
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngAnimate']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: 'home.html',
+    templateUrl: 'page-home.html',
     controller: 'HomeCtrl'
   });
-  $routeProvider.when('/services', {
-    templateUrl: 'services.html',
+  $routeProvider.when('/page-services', {
+    templateUrl: 'page-services.html',
     controller: 'ServicesCtrl'
   });
-  $routeProvider.when('/clients', {
-    templateUrl: 'clients.html',
+  $routeProvider.when('/page-contact', {
+    templateUrl: 'page-contact.html',
     controller: 'ClientsCtrl'
+  });
+    $routeProvider.when('/page-workerscomp', {
+    templateUrl: 'page-workerscomp.html',
+    controller: 'WorkersCtrl'
+  });
+    $routeProvider.when('/page-settings', {
+    templateUrl: 'page-settings.html',
+    controller: 'SettingsCtrl'
   });
   $routeProvider.otherwise({redirecTo: '/'});
 }]);
 
 app.controller('HomeCtrl', ['$scope', function($scope){
     $scope.message = 'JS';
+    $scope.pageClass = 'page-home';
 }]);
 
 app.controller('ServicesCtrl', ['$scope', function($scope) {
     $scope.message = 'Location';
+    $scope.pageClass = 'page-services';
 }]);
 
 app.controller('ClientsCtrl', ['$scope', function($scope) {
-    $scope.message = 'Coming Soon';
+    $scope.message = 'No new messages from nurse at this time';
+    $scope.pageClass = 'page-contact';
 }]);
+
+app.controller('WorkersCtrl', ['$scope', function($scope) {
+    $scope.message = 'No Workers Comp Schedule at this time';
+    $scope.pageClass = 'page-workerscomp';
+}]);
+
+app.controller('SettingsCtrl', ['$scope', function($scope) {
+    $scope.message = 'Martin Sebastian, 813-454-2912, 37417 Laurel Hammock Dr, Zephyrhills Florida 33541';
+    $scope.pageClass = 'page-settings';
+}]);
+
